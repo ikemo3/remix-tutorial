ac80ed680e0e1f9acf8fa300bb90389a22c02922
Hideki Ikemoto <ikemo333@gmail.com>
2023-09-30 08:45:32 +0900

    Active Link Styling

--- app/root.tsx
+++ app/root.tsx
@@ -3,10 +3,10 @@ import { json, redirect } from "@remix-run/node";
 
 import {
   Form,
-  Link,
   Links,
   LiveReload,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -65,7 +65,13 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      {" "}
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -74,7 +80,7 @@ export default function App() {
                         <i>No Name</i>
                       )}{" "}
                       {contact.favorite ? <span>★</span> : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
